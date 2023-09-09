51a1a40be38c119b4b0e51f2b766b493e9d57d78
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-09-09 19:03:28 -0300

    method to sign in as a firebase user inside tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,20 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
+use Kreait\Laravel\Firebase\Facades\Firebase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function actingAsFirebaseUser(String $email, String $plainPassword)
+    {
+        $auth = Firebase::auth();
+        $signInResult = $auth->signInWithEmailAndPassword($email, $plainPassword); 
+
+        $token = $signInResult->idToken();
+        
+        $this->withHeader('Authorization', 'Bearer ' . $token);
+    }
 }

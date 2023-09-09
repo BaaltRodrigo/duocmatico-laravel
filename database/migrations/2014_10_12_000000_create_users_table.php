4b8ef30aa0e373ecb530da960ec5c405caee1785
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-09-09 19:03:55 -0300

    Set string id as a primary key

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            // $table->id();
-            $table->string('id')->unique(); // Firebase UID (unique identifier) for the users
+            $table->string('id')->primary(); // Firebase UID (unique identifier) for the users
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();

bcdee07a1cb073dfc4dca8eb5d22918e9c0dcc12
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-09-13 11:12:58 -0300

    Fix some issue with the local time for tokens

--- app/Http/Middleware/FirebaseAuth.php
+++ app/Http/Middleware/FirebaseAuth.php
@@ -7,6 +7,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Kreait\Laravel\Firebase\Facades\Firebase;
+use Firebase\Auth\Token\Exception\IssuedInTheFuture;
 
 class FirebaseAuth
 {
@@ -32,22 +33,35 @@ class FirebaseAuth
             return response()->json(['status' => 'error','message' => 'Token not provided'], Response::HTTP_UNAUTHORIZED);
         }
 
-        // Verify the token
-        $verifiedIdToken = $this->auth->verifyIdToken($token, $checkIfRevoked = true);
+        try {
+            // Verify the token
+            $verifiedIdToken = $this->auth->verifyIdToken($token, $checkIfRevoked = true);
+        } catch (IssuedInTheFuture $e) {
+            // This solves the issue for now, but we should check the time
+            // on the server and the time on the token
+            $verifiedIdToken = $e->getToken();
+        }
         
         // Set the user in the request and proceed
         $uid = $verifiedIdToken->claims()->get('sub');
         $userPayload = $this->auth->getUser($uid);
         
         // Check if the user exists on the tables
-        // TODO: Move this into an action or something
-        $user = User::firstOrCreate([
-            'id' => $userPayload->uid,
-            'name' => $userPayload->displayName ?? $userPayload->email,
-            'email' => $userPayload->email,
-        ]);
-        
-        $request->user = $user;
+        $user = User::find($userPayload->uid);
+
+        if (!$user) {
+            // No user, create one
+            // TODO: Move this into an action or something
+            $user = User::create([
+                'id' => $userPayload->uid,
+                'name' => $userPayload->displayName ?? $userPayload->email,
+                'email' => $userPayload->email,
+            ]);
+        }
+
+        // At this point, we get an old user or the new one
+        // sign the user in
+        auth()->login($user);     
 
         return $next($request);
     }

282a0cc76f8706bd2e7439d79bad51458c2b42e6
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-09-16 17:46:41 -0300

    Add School models to reduce duplication inside Subjects

---
+++ app/Models/School.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class School extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'name',
+    ];
+
+    public function subjects()
+    {
+        return $this->hasMany(Subject::class);
+    }
+}

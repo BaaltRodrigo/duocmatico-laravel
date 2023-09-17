31132f452a84618e784403dabfe7a3155ba7641b
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-09-16 23:49:51 -0300

    Add schedules to sections

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Section extends Model
 {
@@ -29,5 +30,10 @@ class Section extends Model
     public function subject(): BelongsTo
     {
         return $this->belongsTo(Subject::class);
+    }
+
+    public function schedules(): BelongsToMany
+    {
+        return $this->belongsToMany(Schedule::class);
     }
 }

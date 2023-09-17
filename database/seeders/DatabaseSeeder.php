ff07228174686790285784fa62c7f61712e57e8a
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-09-16 23:26:24 -0300

    Database seeder uses call with an array

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RolesAndPermissionSeeder::class);
+        $this->call([
+            RolesAndPermissionSeeder::class,
+        ]);
     }
 }

ef282f9a77761e8c1e47018940e7ec5ccdc34b78
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-09-22 01:35:23 -0300

    Action to handle a full upload for academic charge csv

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesAndPermissionSeeder::class,
+            AcademicChargeSeeder::class,
         ]);
     }
 }

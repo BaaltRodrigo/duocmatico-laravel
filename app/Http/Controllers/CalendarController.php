669d5113ab0071e61c0ad721caf33abbabd9b2da
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-06 13:07:23 -0300

    Base calendar controller

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCalendarRequest;
+use App\Http\Requests\UpdateCalendarRequest;
+use App\Models\Calendar;
+use Illuminate\Http\Request;
+
+class CalendarController extends Controller
+{
+    public function index(Request $request) {
+        $user = auth()->user();
+
+        return $user->calendars;
+    }
+
+    public function show(Calendar $calendar) {
+        return $calendar;
+    }
+
+    public function store(StoreCalendarRequest $request) 
+    {
+        // Authorization handled inside form request
+        $validated = $request->validated();
+        $calendar = Calendar::create($validated);
+
+        return $calendar;
+    }
+
+    public function update(UpdateCalendarRequest $request, Calendar $calendar)
+    {
+        // Authorization handled inside form request
+        $validated = $request->validated();
+        $calendar->update($validated);
+        return $calendar;
+    }
+
+    public function delete(Calendar $calendar)
+    {
+        $calendar->delete();
+        return response()->json(['message' => 'Calendar deleted']);
+    }
+}

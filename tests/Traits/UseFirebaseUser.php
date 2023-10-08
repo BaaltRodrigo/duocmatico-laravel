31fecf41d7bd1d381d72e06f2b3c1800606bbd5f
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-08 12:33:48 -0300

    Firebase user credentials moved to `.env`
    Generate new credentials to prevent information leak

--- tests/Traits/UseFirebaseUser.php
+++ tests/Traits/UseFirebaseUser.php
@@ -7,17 +7,6 @@ use Kreait\Laravel\Firebase\Facades\Firebase;
 
 trait UseFirebaseUser
 {
-
-    /**
-     * This credentials are the ones from the firebase auth tool.
-     * In case that there is a change in the firebase auth tool,
-     * this credentials should be updated.
-     */
-    public string $user_id = '4qVRFaULHRbXnESK7y0xoHKf02Q2';
-    public string $user_name = 'Test-Duocmatico';
-    public string $user_email = 'duocmatico-admin@example.net';
-    public string $user_password = 'securePassword';
-
     /**
      * Creates the default test user from firebase auth, but
      * changes the roles to the ones provided in the array.
@@ -35,7 +24,7 @@ trait UseFirebaseUser
 
     public function createUser(): User
     {
-        return User::create(['id' => $this->user_id,]);
+        return User::create(['id' => env('FIREBASE_TEST_USER_UID')]);
     }
     
     /**
@@ -44,7 +33,9 @@ trait UseFirebaseUser
     public function actingAsFirebaseUser(): void
     {
         $auth = Firebase::auth();
-        $signInResult = $auth->signInWithEmailAndPassword($this->user_email, $this->user_password); 
+        $email = env('FIREBASE_TEST_USER_EMAIL');
+        $password = env('FIREBASE_TEST_USER_PASSWORD');
+        $signInResult = $auth->signInWithEmailAndPassword($email, $password); 
 
         $token = $signInResult->idToken();
         

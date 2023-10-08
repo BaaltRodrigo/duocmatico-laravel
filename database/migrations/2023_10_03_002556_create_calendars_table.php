d23ea3f1f0cf02b507ebf351729f7b88d4c84828
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2023-10-08 02:43:00 -0300

    Remove default uuid value on calendars

--- database/migrations/2023_10_03_002556_create_calendars_table.php
+++ database/migrations/2023_10_03_002556_create_calendars_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -14,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('calendars', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique()->default(Str::uuid()); // for public purposes
+            $table->string('uuid')->unique(); // for public purposes
             $table->boolean('is_public')->default(false); // for public purposes
             $table->string('name');
             $table->string('description')->nullable();

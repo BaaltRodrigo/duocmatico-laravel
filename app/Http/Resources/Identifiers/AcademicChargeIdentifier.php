3935506685b93d38c1847f6185a2748ecc147514
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2024-09-09 01:18:59 -0300

    feat: :sparkles: academic charges now stores the file used as base

--- app/Http/Resources/Identifiers/AcademicChargeIdentifier.php
+++ app/Http/Resources/Identifiers/AcademicChargeIdentifier.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Identifiers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class AcademicChargeIdentifier extends JsonResource
 {
@@ -14,12 +15,15 @@ class AcademicChargeIdentifier extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
         return [
             'id' => $this->id,
             'name' => str_replace('-', ' ', $this->name),
-            'season' => "{$this->year}-{$this->semester}",
+            'year' => $this->year,
+            'semester' => $this->semester,
             'is_hidden' => $this->is_hidden,
             'url' => route('academic-charges.show', $this->id),
+            'file_path' => Storage::disk('r2')->url($this->path),
         ];
     }
 }

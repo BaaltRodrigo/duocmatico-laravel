a8eaf1f89e1f9a1bf28a027580226ddf32a8db08
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-03-06 22:14:06 -0300

    ✏️ tables should be plural

--- database/migrations/2025_03_07_010424_create_career_table.php
+++ database/migrations/2025_03_07_010424_create_career_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('career', function (Blueprint $table) {
+        Schema::create('careers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->softDeletes();
--- database/migrations/2025_03_07_010830_create_campus_table.php
+++ database/migrations/2025_03_07_010830_create_campus_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('campus', function (Blueprint $table) {
+        Schema::create('campuses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->softDeletes();

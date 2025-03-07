fc4dcf7ff0cc2a23b8cb823f2aa195e51d2d2ed2
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-03-07 00:36:03 -0300

    🚧 section model and migration

---
+++ app/Models/Section.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Section extends Model
+{
+
+    public $fillable = ['code', 'shift', 'course_id'];
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+}

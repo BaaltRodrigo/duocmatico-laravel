26bce0ea65b0daafeaf6066361d1527c43ed8162
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-03-09 00:49:24 -0300

    🩹 add slash to the prefix

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
+Route::prefix('/v1')->group(function () {
     Route::post('academic-charge', UploadAcademicChargeController::class);
 });

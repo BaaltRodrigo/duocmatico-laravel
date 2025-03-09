1b2f6e6ec6c88bfcdd9fb50da09b8f2ac1c2b598
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-03-09 00:57:45 -0300

    ♻️ inverse relationship course-section

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     public $fillable = ['code', 'name', 'level'];
+
+    public function sections(): HasMany
+    {
+        return $this->hasMany(Section::class);
+    }
 }

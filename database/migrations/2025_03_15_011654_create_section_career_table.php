0ac04c016c80ba13ba49e733fb66a8dad99cda48
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-03-14 22:20:53 -0300

    🗃️ relationship of section with campus and career

---
+++ database/migrations/2025_03_15_011654_create_section_career_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('section_career', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('section_id')->constrained();
+            $table->foreignId('career_id')->constrained();
+
+            $table->unique(['section_id', 'career_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('section_career');
+    }
+};

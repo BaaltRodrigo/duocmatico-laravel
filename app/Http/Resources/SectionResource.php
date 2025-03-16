0cbfa222e0dabd07e3f13ea68de6e6f943d2c02d
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-03-16 01:47:08 -0300

    ✨ resources for sections and schedules

---
+++ app/Http/Resources/SectionResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SectionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'year' => $this->year,
+            'code' => $this->code,
+            'shift' => $this->shift,
+            'season' => $this->season,
+            'schedules' => ScheduleResource::collection($this->schedules),
+            'careers' => CareerIdentifierResource::collection($this->careers),
+        ];
+    }
+}

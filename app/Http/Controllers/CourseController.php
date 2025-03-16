9c5d92f7f0556ff380638872220313351712bd5b
Rodrigo Pizarro <r.antonio.regueiro@gmail.com>
2025-03-16 01:42:10 -0300

    ✨ course index filters by year and season

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -10,7 +10,19 @@ class CourseController extends Controller
 {
     public function index(Request $request)
     {
-        $courses = Course::with(['sections','sections.schedules'])->get();
+        // this route will get 2 query params as mandatory
+        // 1. The year we need to fetch the course sections
+        // 2. The season of the year we need to fetch the course sections
+        // Example: /api/courses?year=2021&season=1
+        // This will return all the courses with their sections and schedules for the year 2021 and season 1
+
+        $year = $request->input('year');
+        $season = $request->input('season');
+
+        $courses = Course::with(['sections.schedules'])->whereHas('sections', function ($query) use ($year, $season) {
+            $query->where('year', $year)->where('season', $season);
+        })->get();
+
         $resources = new CourseCollection($courses);
 
         return $resources;
